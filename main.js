b241b0980ef26d62e2540f903c7c4dd0b5d9fb37
AironRuda <airon.ruda@udea.edu.co>
2022-11-23 18:44:58 -0500

    codigo refinado, faltan metodos get, post... para crear nuevos productos y mostrar ordenes

--- main.js
+++ main.js
@@ -33,48 +33,44 @@ const getInfo = async (browser) => {
  */
 const createBasicCard = (data) => {
   cardField.innerHTML += `
-  <div class="col">
-    <div class="card" id="${data.id}" style="width: 18rem">
-        <img src="${data.img}" class="card-img-top" alt="..." />
-        <div class="card-body">
-            <p class="card-text">${data.type}</p>
-            <h5 class="card-title">${data.name}</h5>
-        </div>
-        <div class="money_bag">
-              <h3>$ ${data.price}</h3>
-              <button class="btn btn-success text-uppercase btn-sm details cart-btn" onClick="itemSelected(${data.id} ,'cart')">
-                ADD
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-cart3"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                  />
-                </svg>
-              </button>
-              <button class="btn btn-success text-uppercase btn-sm details favs-btn" onClick="itemSelected(${data.id} ,'fav')">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-heart-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                  />
-                </svg>
-              </button>
-            </div>
+  <div class="card">
+    <img src="${data.img}" class="card-img" alt="${data.name}" />
+    <p class="productType">${data.type}</p>
+    <p class="productName"><strong>${data.name}</strong></p>
+    <h3 class="productPrice">$ ${data.price}</h3>
+    <div class="btn-filed">
+      <button class="btn btn-success text-uppercase btn-sm details cart-btn" onClick="itemSelected(${data.id} ,'cart')">
+        ADD
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-cart3"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+          />
+        </svg>
+      </button>
+      <button class="btn btn-success text-uppercase btn-sm details favs-btn" onClick="itemSelected(${data.id} ,'fav')">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-heart-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+          />
+        </svg>
+      </button>
     </div>
-    </div>
+  </div>
   `;
 };
 /**
@@ -102,25 +98,29 @@ browser.addEventListener("submit", (e) => {
   showProducts(e.target.search.value);
 });
 /**
- * recibe un id como parametro, extrae el objeto con el id del db y lo agrega al loca storage
- * @param {*str} idSelected
- * @param {*str} favOrCart
+ * extra la informacion del localStorage
+ * @param {*str} favOrCart "fav" or "cart"
+ * @returns array con objetos disponibles en el localStorage
+ */
+const extractInfoFromLocalStorage = (favOrCart) => {
+  let favOrCartStorage = localStorage.getItem(favOrCart); // trae del local storage o el carrito o el favPage
+  let infoStorage;
+  favOrCartStorage
+    ? (infoStorage = JSON.parse(favOrCartStorage))
+    : (infoStorage = []);
+  return infoStorage;
+};
+/**
+ * recibe un id como parametro, extrae el objeto con el id del db y lo agrega al localStorage
+ * @param {*str} idSelected id del producto que se agregará a una de las listas disponibles
+ * @param {*str} favOrCart "fav" or "cart"
  */
 const itemSelected = async (idSelected, favOrCart) => {
   const data = await getInfo();
   const product = data.find((item) => item.id == idSelected); // extrae el producto con el id
-
-  let favOrCartStorage = localStorage.getItem(favOrCart); // trae del local storage o el carrito o el favPage
-  let infoStorage;
-
-  favOrCartStorage
-    ? (infoStorage = JSON.parse(favOrCartStorage))
-    : (infoStorage = []);
-
+  let infoStorage = extractInfoFromLocalStorage(favOrCart);
   let bandFinded = infoStorage.some((item) => item.id == idSelected);
-
   bandFinded ? console.log("Ya está en " + "fav") : infoStorage.push(product);
-
   let answerJSON = JSON.stringify(infoStorage);
   localStorage.setItem(favOrCart, answerJSON);
   addInfoToCart();
@@ -131,32 +131,29 @@ const itemSelected = async (idSelected, favOrCart) => {
  */
 const createFavCard = (data) => {
   cardField.innerHTML += `
-    <div class="card" id="${data.id}" style="width: 18rem">
-      <button type="button" class="btn-close" aria-label="Close" onClick="deleteItem(${data.id},'fav')"></button>
-      <img src="${data.img}" class="card-img-top" alt="..." />
-      <div class="card-body">
-          <p class="card-text">${data.type}</p>
-          <h5 class="card-title">${data.name}</h5>
+    <div class="card">
+      <button class="btn-close" onClick="deleteItem(${data.id},'fav')"></button>
+      <img src="${data.img}" class="card-img" alt="${data.name}" />
+      <p class="productType">${data.type}</p>
+      <p class="productName"><strong>${data.name}</strong></p>
+      <h3 class="productPrice">$ ${data.price}</h3>
+      <div class="btn-filed">
+        <button class="btn btn-success text-uppercase btn-sm details cart-btn" onClick="itemSelected(${data.id} ,'cart')">
+          ADD
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-cart3"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
       </div>
-      <div class="money_bag">
-            <h3>$ ${data.price}</h3>
-            <button class="btn btn-success text-uppercase btn-sm details cart-btn" onClick="itemSelected(${data.id} ,'cart')">
-              ADD
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-cart3"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                />
-              </svg>
-            </button>
-      
-          </div>
     </div>
   `;
 };
@@ -164,10 +161,8 @@ const createFavCard = (data) => {
  * ingresa al localstorage de fav y los muestra en pantalla
  */
 const showFavs = () => {
-  cardField.innerHTML = `<h1>Favoritos</h1>`;
-  let favStorage = localStorage.getItem("fav");
-  let infoStorage;
-  favStorage ? (infoStorage = JSON.parse(favStorage)) : (infoStorage = []);
+  cardField.innerHTML = `<h1 class="section-title">Favoritos</h1><br>`;
+  let infoStorage = extractInfoFromLocalStorage("fav");
   infoStorage.forEach((element) => {
     createFavCard(element);
   });
@@ -179,65 +174,159 @@ const showFavs = () => {
 
 const createCartCard = (data) => {
   dropdownContent.innerHTML += `
-    <div class="card" id="${data.id}" style="width: 18rem">
-    <button type="button" class="btn-close" aria-label="Close" onClick="deleteItem(${data.id}, 'cart')"></button>
-      <img src="${data.img}" class="card-img-top" alt="..." />
-      <div class="card-body">
-          <p class="card-text">${data.type}</p>
-          <p class="card-title">${data.name}</p>
-      </div>
-      <div class="money_bag">
-            <h3>$ ${data.price}</h3>
+    <div class="cardCart">
+      <button class="btn-close btn-close-cart" onClick="deleteItem(${data.id}, 'cart')"></button>
+      <img src="${data.img}" class="card-img-cart" alt="${data.name}" />
+      <div class="card-cart-info">
+        <p class="productType">${data.type}</p>
+        <p class="productName"><strong>${data.name}</strong></p>
       </div>
       
+      <h3 class="productPrice-cart">$ ${data.price}</h3>
     </div>
   `;
 };
-
+/**
+ * Agregar informacion carrito
+ */
 const addInfoToCart = () => {
   dropdownContent.innerHTML = `
   <h1>Carrito</h1>
   `;
-  let cartStorage = localStorage.getItem("cart");
-  let infoStorage;
-  cartStorage ? (infoStorage = JSON.parse(cartStorage)) : (infoStorage = []);
+  let infoStorage = extractInfoFromLocalStorage("cart");
+  const cartIndex = document.querySelector("#cartIndex");
+  cartIndex.innerHTML = infoStorage.length;
   infoStorage.forEach((element) => {
     createCartCard(element);
   });
   dropdownContent.innerHTML += `
-    <button type="button" class="btn btn-primary btn-sm">Small button</button>
-    <button type="button" class="btn btn-secondary btn-sm">Small button</button>
+    <button class="btn btn-success text-uppercase btn-cart-view" onclick="confirmCartView()">Confirmar compra</button>
+    
   `;
 };
-
-const deleteItem = (idToDelete, favOrCart) => {
-  let storage = localStorage.getItem(favOrCart);
-  let infoStorage = JSON.parse(storage);
+/**
+ * Se debe proporcionar un entero que indica el id del objeto a eliminar, y de donde se desea eliminar
+ * @param {*int} idToDelete
+ * @param {*string} favOrCart "fav" or "cart"
+ */
+const deleteItem = (idToDelete, favOrCart, cartConfirmRefresh) => {
+  let infoStorage = extractInfoFromLocalStorage(favOrCart);
   infoStorage = infoStorage.filter((item) => item.id != idToDelete);
   let answerJSON = JSON.stringify(infoStorage);
   localStorage.setItem(favOrCart, answerJSON);
-  showFavs();
-  addInfoToCart();
-};
-/*
 
-const deleteItem = (idToDelete) => {
-  let favStorage = localStorage.getItem("fav");
-  let infoStorage = JSON.parse(favStorage);
-  infoStorage = infoStorage.filter((item) => item.id != idToDelete);
-  let answerJSON = JSON.stringify(infoStorage);
-  localStorage.setItem("fav", answerJSON);
-  showFavs();
+  if (favOrCart == "fav") {
+    showFavs();
+  } else {
+    addInfoToCart();
+    if (cartConfirmRefresh) confirmCartView();
+  }
+  // ;
+};
+/**
+ * crea cartas que esten en el carrito de compras
+ * @param {*object} data
+ */
+const createCartCardToConfirm = (data) => {
+  cardField.innerHTML += `
+    <div class="cardCartConfirm">
+      <img src="${data.img}" class="card-img-confirm" alt="${data.name}" />
+      <div>
+        <p class="productType">${data.type}</p>
+        <p class="productName"><strong>${data.name}</strong></p>
+      </div>
+      <h3 class="productPrice">$ ${data.price}</h3>
+
+      <div class="quantity-btn-field">
+        <button onclick="modifyQuantityProduct(${data.id},'plus')">+</button>
+        <P>${data.quantity}</P>
+        <button onclick="modifyQuantityProduct(${data.id},'minus')">-</button>
+      </div>
+      <div>
+        <p>
+        total
+        ${data.quantity * data.price}
+        </p>
+      </div>
+      <button class="btn-close" aria-label="Close" onClick="deleteItem(${
+        data.id
+      }, 'cart', true)"></button>
+    </div>
+  `;
 };
 
-const showFavs = () => {
-  cardField.innerHTML = `<h1>Favoritos</h1>`;
-  let favStorage = localStorage.getItem("fav");
-  let infoStorage;
-  favStorage ? (infoStorage = JSON.parse(favStorage)) : (infoStorage = []);
+/**
+ * Extrae los productos del carrito de compras, los muestra y genera una suma del total de la compra
+ */
+const confirmCartView = () => {
+  cardField.innerHTML = `<h1 class="section-title">Orden de compra</h1><br>`;
+  let infoStorage = extractInfoFromLocalStorage("cart");
+  let total = 0;
   infoStorage.forEach((element) => {
-    createFavCard(element);
+    total += element.quantity * element.price;
+    createCartCardToConfirm(element);
   });
+  cardField.innerHTML += `
+    <div class="total-cart text-uppercase">
+      <p>
+      <strong>total de compra: $ ${total}</strong>
+      </p>
+    </div>
+    <button class="btn btn-success" onClick="confirmCartOrder()">Confirmar compra</button>
+  `;
 };
+/**
+ * modifica en una unidad (mas o menos, dependiendo sel segundo parametro) la cantidad del producto con id proporcionada
+ * @param {*int} idToModiifyQuantity id de producto a modificar
+ * @param {*str} plusOrMinus "plus" or "minus"
+ */
+const modifyQuantityProduct = (idToModiifyQuantity, plusOrMinus) => {
+  let infoStorage = extractInfoFromLocalStorage("cart");
+  infoStorage.forEach((element) => {
+    if (element.id == idToModiifyQuantity) {
+      plusOrMinus == "plus" ? (element.quantity += 1) : (element.quantity -= 1);
+      if (element.quantity < 0) {
+        element.quantity = 0;
+      }
+    }
+  });
+  let answerJSON = JSON.stringify(infoStorage);
+  localStorage.setItem("cart", answerJSON);
+  confirmCartView();
+};
+const showFormCompleteOrderCart = () => {
+  cardField.innerHTML += `
+  <form class="form-group">
+    <input type="text" id="username" class="form-control mt-2" placeholder="Nombre" required>
+    <input type="text" id="addres" class="form-control mt-2" placeholder="direccion" required>
+    <input type="email" id="email" class="form-control mt-2" placeholder="e-mail" required>
+  </form>
+  <button class="btn btn-success" onClick="getInfoToConfirm()">Completar compra</button> 
+  `;
+};
+
+const confirmCartOrder = () => {
+  cardField.innerHTML = `<h1 class="section-title">Confirmar orden de compra</h1><br>`;
+  showFormCompleteOrderCart();
+};
+const getInfoToConfirm = () => {
+  const username = document.getElementById("username");
+  const addres = document.getElementById("addres");
+  const email = document.getElementById("email");
+  const infoStorage = extractInfoFromLocalStorage("cart");
+
+  let object = {
+    username: username.value,
+    addres: addres.value,
+    email: email.value,
+    purchase: infoStorage,
+  };
+  // let answerJSON = JSON.stringify([]);
+  // localStorage.setItem(favOrCart, answerJSON);
+  console.log(object);
+  showProducts();
+};
+
+/*
 
 */
